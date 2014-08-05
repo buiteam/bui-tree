ad68397e1d596d1b2f82e32d6101f4b07ff9a8c3
索丘 <zengyue.yezy@alibaba-inc.com>
2014-08-05 21:15:47 +0800

    重新打包

--- dist/bui-tree/1.1.0/index-debug.js
+++ dist/bui-tree/1.1.0/index-debug.js
@@ -1,4 +1,4 @@
-define("bui-tree/1.1.0/index-debug", ["jquery", "bui-common/1.1.0/common-debug", "bui-tree/1.1.0/src/treelist-debug", "bui-tree/1.1.0/src/treemixin-debug", "bui-tree/1.1.0/src/treemenu-debug", "bui-data/1.1.0/index-debug", "bui-list/1.1.0/index-debug", "bui-tree/1.1.0/src/selection-debug"], function(require, exports, module) {
+define("bui-tree/1.1.0/index-debug", ["bui-common/1.1.0/common-debug", "jquery", "bui-list/1.1.0/index-debug", "bui-data/1.1.0/index-debug"], function(require, exports, module) {
   /**
    * @fileOverview 选择框命名空间入口文件
    * @ignore
@@ -12,7 +12,7 @@ define("bui-tree/1.1.0/index-debug", ["jquery", "bui-common/1.1.0/common-debug",
   });
   module.exports = Tree;
 });
-define("bui-tree/1.1.0/src/treelist-debug", ["jquery", "bui-common/1.1.0/common-debug", "bui-data/1.1.0/index-debug", "bui-list/1.1.0/index-debug", "bui-tree/1.1.0/src/treemixin-debug", "bui-tree/1.1.0/src/selection-debug"], function(require, exports, module) {
+define("bui-tree/1.1.0/src/treelist-debug", ["bui-common/1.1.0/common-debug", "jquery", "bui-list/1.1.0/index-debug", "bui-data/1.1.0/index-debug"], function(require, exports, module) {
   /**
    * @fileOverview 树形列表
    * @ignore
@@ -129,7 +129,7 @@ define("bui-tree/1.1.0/src/treemixin-debug", ["jquery", "bui-common/1.1.0/common
       fn();
     }, timeout / count);
   }
-  var $ = require("jquery"),
+  var $ = require('jquery'),
     BUI = require("bui-common/1.1.0/common-debug"),
     Data = require("bui-data/1.1.0/index-debug"),
     EXPAND = 'expanded',
@@ -1521,7 +1521,56 @@ define("bui-tree/1.1.0/src/treemixin-debug", ["jquery", "bui-common/1.1.0/common
   });
   module.exports = Mixin;
 });
-define("bui-tree/1.1.0/src/treemenu-debug", ["jquery", "bui-common/1.1.0/common-debug", "bui-data/1.1.0/index-debug", "bui-list/1.1.0/index-debug", "bui-tree/1.1.0/src/treemixin-debug", "bui-tree/1.1.0/src/selection-debug"], function(require, exports, module) {
+define("bui-tree/1.1.0/src/selection-debug", ["bui-common/1.1.0/common-debug", "jquery", "bui-list/1.1.0/index-debug", "bui-data/1.1.0/index-debug"], function(require, exports, module) {
+  /**
+   * @fileOverview 树的选中，跟列表的选中有所差异
+   * @ignore
+   */
+  var BUI = require("bui-common/1.1.0/common-debug"),
+    SimpleList = require("bui-list/1.1.0/index-debug").SimpleList;
+  /**
+   * @class BUI.Tree.Selection
+   * 扩展树的选择
+   */
+  var Selection = function() {};
+  Selection.ATTRS = {};
+  BUI.augment(Selection, {
+    /**
+     * 获取选中的节点，一般用于多选状态下
+     * @return {Array} 获取选中的节点
+     */
+    getSelection: function() {
+      var _self = this,
+        field = _self.getStatusField('selected'),
+        store;
+      if (field) {
+        store = _self.get('store');
+        return store.findNodesBy(function(node) {
+          return node[field];
+        });
+      }
+      return SimpleList.prototype.getSelection.call(this);
+    },
+    /**
+     * 获取选中的一个节点，如果是多选则返回第一个
+     * @return {Object} 获取选中的一个节点
+     */
+    getSelected: function() {
+      var _self = this,
+        field = _self.getStatusField('selected'),
+        store;
+      if (field) {
+        store = _self.get('store');
+        return store.findNodeBy(function(node) {
+          return node[field];
+        });
+      }
+      return SimpleList.prototype.getSelected.call(this);
+    }
+  });
+  module.exports = Selection;
+});
+define("bui-tree/1.1.0/src/treemenu-debug", ["bui-common/1.1.0/common-debug", "jquery", "bui-list/1.1.0/index-debug", "bui-data/1.1.0/index-debug"], function(require, exports, module) {
   /**
    * @fileOverview 树形菜单
    * @ignore
@@ -1640,53 +1689,4 @@ define("bui-tree/1.1.0/src/treemenu-debug", ["jquery", "bui-common/1.1.0/common-
   });
   TreeMenu.View = TreeMenuView;
   module.exports = TreeMenu;
-});
-define("bui-tree/1.1.0/src/selection-debug", ["jquery", "bui-common/1.1.0/common-debug", "bui-data/1.1.0/index-debug", "bui-list/1.1.0/index-debug"], function(require, exports, module) {
-  /**
-   * @fileOverview 树的选中，跟列表的选中有所差异
-   * @ignore
-   */
-  var BUI = require("bui-common/1.1.0/common-debug"),
-    SimpleList = require("bui-list/1.1.0/index-debug").SimpleList;
-  /**
-   * @class BUI.Tree.Selection
-   * 扩展树的选择
-   */
-  var Selection = function() {};
-  Selection.ATTRS = {};
-  BUI.augment(Selection, {
-    /**
-     * 获取选中的节点，一般用于多选状态下
-     * @return {Array} 获取选中的节点
-     */
-    getSelection: function() {
-      var _self = this,
-        field = _self.getStatusField('selected'),
-        store;
-      if (field) {
-        store = _self.get('store');
-        return store.findNodesBy(function(node) {
-          return node[field];
-        });
-      }
-      return SimpleList.prototype.getSelection.call(this);
-    },
-    /**
-     * 获取选中的一个节点，如果是多选则返回第一个
-     * @return {Object} 获取选中的一个节点
-     */
-    getSelected: function() {
-      var _self = this,
-        field = _self.getStatusField('selected'),
-        store;
-      if (field) {
-        store = _self.get('store');
-        return store.findNodeBy(function(node) {
-          return node[field];
-        });
-      }
-      return SimpleList.prototype.getSelected.call(this);
-    }
-  });
-  module.exports = Selection;
 });
